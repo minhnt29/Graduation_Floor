15934a16a1a617eabe1f52298dcc92591bbf94ea
minhnt29 <nguyentuanminh2910bk@gmail.com>
2022-10-03 17:22:28 +0700

    config some function

--- main/app_main.c
+++ main/app_main.c
@@ -32,16 +32,58 @@
 
 #include "DHT22.h"
 
-static const char *TAG = "MQTT_EXAMPLE";
+#define BROKER       "test.mosquitto.org"   
+#define TOPIC_DOOR  "Prj/Door"
+#define TOPIC_FLOOR1  "Prj/Floor1"
+#define TOPIC_FLOOR2  "Prj/Floor2"
+#define TOPIC_FLOOR3  "Prj/Floor3"
+#define TOPIC_ACCOUNT  "Prj/Account"
+#define TOPIC_PASSWORD  "Prj/Password"
+#define TOPIC_FIREALARM "Prj/Fire"
+
+#define DOOR_CMD_LIGHT               '1'
+#define DOOR_CMD_LIGHT_ON            '1'
+#define DOOR_CMD_LIGHT_OFF           '0'
+#define DOOR_CMD_DOOR                '2'
+#define DOOR_CMD_DOOR_OPEN           '1'
+#define DOOR_CMD_DOOR_CLOSE          '0'
+#define DOOR_CMD_DOOR_KEYPAD         '3'
+#define DOOR_CMD_DOOR_CHANGEPASSWORD '0'
+#define DOOR_CMD_DOOR_ADDACCOUNT     '1'
+
+#define FLOOR1_CMD_LIGHT        '4'
+#define FLOOR2_CMD_LIGHT_ON     '1'
+#define FLOOR2_CMD_LIGHT_OFF    '0'
+
+esp_mqtt_client_handle_t client;
+
+static const char *TAG = "Graduation Floor";
+static QueueHandle_t DHT_Temp_queue = NULL,
+                     DHT_Hum_queue = NULL,
+                     Gas_value = NULL;
 
 
-static void log_error_if_nonzero(const char *message, int error_code)
+static void get_DHT_Value(void* arg)
 {
-    if (error_code != 0) {
-        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    uint32_t io_num = 0;
+    for(;;) {
+        // if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            io_num++;
+            xQueueSend(DHT_Temp_queue, &io_num, 1000/portTICK_PERIOD_MS);
+            vTaskDelay(1000);
+        // }
     }
 }
 
+static void send_DHT_Value(void* arg)
+{
+    uint32_t io_num = 0;
+    for(;;) {
+         if(xQueueReceive(DHT_Temp_queue, &io_num, portMAX_DELAY)) {
+            esp_mqtt_client_publish(client, TOPIC_FLOOR1, &io_num, 1, 1, 0);
+         }
+    }
+}
 /*
  * @brief Event handler registered to receive MQTT events
  *
@@ -61,47 +103,14 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
 
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+        msg_id = esp_mqtt_client_subscribe(client, TOPIC_FLOOR1, 1);
         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
-        break;
-    case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        break;
-
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-        break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        break;
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
         printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
         printf("DATA=%.*s\r\n", event->data_len, event->data);
-        break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-
-        }
         break;
     default:
         ESP_LOGI(TAG, "Other event id:%d", event->event_id);
@@ -112,53 +121,18 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 static void mqtt_app_start(void)
 {
     esp_mqtt_client_config_t mqtt_cfg = {
-        .uri = CONFIG_BROKER_URL,
+        .uri = BROKER,
     };
-#if CONFIG_BROKER_URL_FROM_STDIN
-    char line[128];
 
-    if (strcmp(mqtt_cfg.uri, "FROM_STDIN") == 0) {
-        int count = 0;
-        printf("Please enter url of mqtt broker\n");
-        while (count < 128) {
-            int c = fgetc(stdin);
-            if (c == '\n') {
-                line[count] = '\0';
-                break;
-            } else if (c > 0 && c < 127) {
-                line[count] = c;
-                ++count;
-            }
-            vTaskDelay(10 / portTICK_PERIOD_MS);
-        }
-        mqtt_cfg.uri = line;
-        printf("Broker url: %s\n", line);
-    } else {
-        ESP_LOGE(TAG, "Configuration mismatch: wrong broker url");
-        abort();
-    }
-#endif /* CONFIG_BROKER_URL_FROM_STDIN */
-
-    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    client = esp_mqtt_client_init(&mqtt_cfg);
     /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
 }
 
+
 void app_main(void)
 {
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
-
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
-    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
-    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
-    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
-
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -170,4 +144,6 @@ void app_main(void)
     ESP_ERROR_CHECK(example_connect());
 
     mqtt_app_start();
+    xTaskCreate(get_DHT_Value, "gpio_task_example", 2048, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(send_DHT_Value, "gpio_task_example", 2048, NULL, configMAX_PRIORITIES, NULL);
 }
